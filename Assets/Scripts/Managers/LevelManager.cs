fd6c666088b407ea25be0a4340140f7a482ecc29
Vaibhav Kokare <vaibhavkokare97@gmail.com>
2024-08-03 20:59:46 +0530

    particle system created

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : Singleton<LevelManager>
 {
     public List<LevelData> levelDatas = new List<LevelData>();
 }
